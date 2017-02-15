398672c0395450bd7eefdad56de75f632ef4dbce
Humberto Plaza Martinez <hplazamartinez@csumb.edu>
2017-02-15 02:33:51 +0000

    ZER0

--- ledLetters.php
+++ ledLetters.php
@@ -237,9 +237,23 @@ function drawLetter($letter, $color) {
 					}
 					break;
  				case "0" :
-					if (1) {
+					if ($i == 1 && $j == 6 || $i == 2 && $j == 5 || $i == 3 && $j == 4 || $i == 4 && $j == 3 || $i == 5 && $j == 2 || $i == 6 && $j == 1) {
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
+					
+					}
+					else if(  $i == 0 && $j == 2 ||  $i == 0 && $j == 3 ||  $i == 0 && $j == 4 ||  $i == 0 && $j == 5  || $i == 7 && $j == 2 ||  $i == 7 && $j == 3 ||  $i == 7 && $j == 4 ||  $i == 7 && $j == 5 )
+					{
+						
+						$colorToDisplay = $color;
+						$letterToDisplay = $letter;
+					}
+					else if($i == 1 && $j == 1 || $i == 2 && $j == 1 || $i == 3 && $j == 1 || $i == 4 && $j == 1 || $i == 5 && $j == 1 || 
+					$i == 6 && $j == 6 || $i == 5 && $j == 6 || $i == 4 && $j == 6 || $i == 3 && $j == 6 || $i == 2 && $j == 6 )
+					{
+						$colorToDisplay = $color;
+						$letterToDisplay = $letter;
+						
 					}
 					break;					
 				case "1" :
