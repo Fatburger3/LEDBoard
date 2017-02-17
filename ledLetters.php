6d8aebeb61d8527934a56508d6259c149bd7115b
Carsen Yates <cyates@csumb.edu>
2017-02-17 00:22:17 +0000

    changed Q

--- ledLetters.php
+++ ledLetters.php
@@ -145,6 +145,7 @@ function drawLetter($letter, $color) {
 				case "Q" :
 					
 					if ($i <= 1 || $i >= 6 || $j <= 1 || $j >= 6) {
+						if(($i == 0 && $j == 6) || ($i == 1 && $j == 7))
 						$colorToDisplay = $color;
 						$letterToDisplay = $letter;
 					}
